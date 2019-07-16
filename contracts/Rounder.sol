5cf09d1605c9ea5d19fcad86bd87e6314373bf89
9areeba <areeba-bito@hotmail.com>
2019-07-16 17:49:38 +0100

    Add two more functions

--- contracts/Rounder.sol
+++ contracts/Rounder.sol
@@ -2,12 +2,20 @@ pragma solidity ^0.5.10;
 
 contract Rounder {
 
-    constructor() public {}
+    address public owner;
 
-    function returnInput(uint256 input)
-        public pure
-        returns (uint256)
-    {
+    constructor() public {
+        owner = msg.sender;
+
+    }
+
+    function returnInput(uint256 input) public pure returns (uint256) {
         return input;
+    }
+
+
+    function ChangeOwner(address owner2) public returns(address) {
+        owner = owner2;
+       // return owner;
     }
 }
--- test/functionTest.js
+++ test/functionTest.js
@@ -1,15 +1,17 @@
+
 const rounder = artifacts.require('Rounder');
 
-contract('Contract Test', function (accounts) {
+contract('Contract Test', function (accounts) { //accunts has a list of accounts being used in gnache cli
 
     describe('Functions', function () {
         let testContract;
+        let deployer = accounts[0];
 
         beforeEach(async function () {
             testContract = await rounder.new(
                 {
-                    from: accounts[0]
-                }
+                    //from account[0]
+                    from: deployer                }
             );
         });
 
@@ -19,6 +21,21 @@ contract('Contract Test', function (accounts) {
             let tx = await testContract.returnInput(expectedOutput);
             assert.equal(tx, expectedOutput);
         });
+
+
+        it('Should set the deployer to owner', async function () {
+            let result = await testContract.owner();
+            assert.equal(result, deployer);
+            
+        });
+
+        it('Should change the address of the owner to newOwner', async function () {
+            let newOwner = accounts[3]; //using the address of accounts[3]
+            let result1 = await testContract.ChangeOwner(newOwner);
+            let result = await testContract.owner();
+            assert.equal(result, newOwner);
+        });
+
 
     });
 });

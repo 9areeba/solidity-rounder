511f3b206a406d6460c59fbe9aabf325801d132e
9areeba <areeba-bito@hotmail.com>
2019-07-17 14:59:23 +0100

    Rounding functions hardcoded to 5

--- contracts/Rounder.sol
+++ contracts/Rounder.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.5.10;
 contract Rounder {
 
     address public owner;
+    uint public gran;
+    uint public output;
 
     constructor() public {
         owner = msg.sender;
+        gran = 5;
 
     }
 
@@ -16,6 +19,23 @@ contract Rounder {
 
     function ChangeOwner(address owner2) public returns(address) {
         owner = owner2;
-       // return owner;
     }
+
+    //rounding accoridng to the granularity
+    function round(uint number) public returns(uint) {
+        uint remainder;
+
+        remainder = number % gran;
+        if (remainder == 0) {
+            output = number;
+        } else {
+            output = number + (gran - remainder);
+        }
+        
+        return output;
+    }
+
+
+
 }
+
--- test/functionTest.js
+++ test/functionTest.js
@@ -1,14 +1,20 @@
 
-const rounder = artifacts.require('Rounder');
+//artifacts.require() teling the test to use the contract "Rounder"
+const rounder = artifacts.require('Rounder'); 
 
+//contract() gives a list of accounts to use within the tests 
 contract('Contract Test', function (accounts) { //accunts has a list of accounts being used in gnache cli
 
     describe('Functions', function () {
+        //these variables are available to any 'it' block within the test suite
         let testContract;
         let deployer = accounts[0];
-
+        
+        //beforeEach test this function is run
         beforeEach(async function () {
-            testContract = await rounder.new(
+            //a new version of contract is deployed to allow tests to be run on a clean contract
+            testContract = await rounder.new( 
+
                 {
                     //from account[0]
                     from: deployer                }
@@ -18,22 +24,50 @@ contract('Contract Test', function (accounts) { //accunts has a list of accounts
 
         it('Should return the input', async function () {
             expectedOutput = 15;
-            let tx = await testContract.returnInput(expectedOutput);
+            let tx = await testContract.returnInput(expectedOutput); //using the functin retrunInput from the Rounder contract
             assert.equal(tx, expectedOutput);
         });
 
 
         it('Should set the deployer to owner', async function () {
-            let result = await testContract.owner(); //jsut a global variable so that's why result is not a transaction value.
+            let result = await testContract.owner(); //just a global variable so that's why result is not a transaction value.
             assert.equal(result, deployer);
             
         });
 
         it('Should change the address of the owner to newOwner', async function () {
             let newOwner = accounts[3]; //using the address of accounts[3]
-            let result1 = await testContract.ChangeOwner(newOwner); //This is the value of the transaction
+            let result1 = await testContract.ChangeOwner(newOwner); //<---This is the value of the transaction
             let result = await testContract.owner();
             assert.equal(result, newOwner);
+        });
+
+
+        it('Should set granularity to 5', async function () {
+            let result = await testContract.gran(); //just a global variable so that's why result is not a transaction value.
+            assert.equal(result, 5);
+        });
+
+
+        it('Should round 7 to granularity', async function () {
+            let expectedNumber = 10
+            let result1 = await testContract.round(7);
+            let output = await testContract.output();
+            assert.equal(output, expectedNumber);
+        });
+
+        it('Should round 5 to granularity', async function () {
+            let expectedNumber = 5
+            let result1 = await testContract.round(5);
+            let output = await testContract.output();
+            assert.equal(output, expectedNumber);
+        });
+
+        it('Should round 1 to granularity', async function () {
+            let expectedNumber = 5
+            let result1 = await testContract.round(1);
+            let output = await testContract.output();
+            assert.equal(output, expectedNumber);
         });
 
 

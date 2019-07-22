507329e0ee507ce8237cdc17bff77bc29e398568
9areeba <areeba-bito@hotmail.com>
2019-07-22 16:52:37 +0100

    Now have a function for making a transaction and also use the SafeMath library

--- contracts/Rounder.sol
+++ contracts/Rounder.sol
@@ -1,38 +1,49 @@
 pragma solidity ^0.5.10;
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
+/// @title A contract that allows a transaction to be made between a pair of couterparties
+/// @author Areeba Safdar
 contract Rounder {
 
-    //public variables allows them to have their on getter function
     address public owner;
     uint public granularity;
     uint public output;
     mapping(address => uint) public balance;
     
-
+    /// @dev construcutor initialises the paramters of the Rounder Contract and
+    /// sets an initial value for the granularity.
+    /// @param account1 an address for one of counterparties using this contract
+    /// @param account2 an addresss for one of the counteroarites using this contract
     constructor(address account1, address account2) public {
         owner = msg.sender;
         granularity = 5;
+        //setting balances for some of the counterparties
         balance[account1] = 50;
         balance[account2] = 20;
     }
 
-
+    /// @dev modifier to check only the owner has access to a function.
     modifier Owner() {
         //This statement needs to be fufilled or else the error message is displayed
         require(msg.sender == owner, "You need to be the owner to change granularity");
-        _; //Allows the rest of the code in the function to be run if the condiition is met
+        _;
     }
-    
+
+    /// @dev only allows the owner to change the value of the granularity by using the modifier.
     function setGranularity(uint num) public Owner() {
         granularity = num;
 
     }
 
+    /// @dev allowing the owner to change.
+    /// @param owner2 the address of the new owner
     function ChangeOwner(address owner2) public returns(address) {
         owner = owner2;
     }
 
-    //rounding accoridng to the granularity
+    /// @dev rounding numbers accoridng to the granularity
+    /// @param number is the value being chekced if it complies with the granularity
+    /// @return value of number complying with the granularity
     function round(uint number) public returns(uint) {
         uint remainder;
 
@@ -42,11 +53,21 @@ contract Rounder {
         } else {
             output = number + (granularity - remainder);
         }
-
         return output;
     }
 
+    /// @dev tranferring money between two counterparties through the use of SafeMath library
+    /// @param value is the amount the sender wants to transfer
+    /// @param sender the address of the person wanting to transfer money
+    function transferBalance(uint value, address sender, address reciever) public {
+        //checking if the amount wanting to be transferred rounds to the granularity
+        uint amount = round(value);
 
+        require(balance[sender] > 0, "You are bankrupt!");
+        require(amount <= balance[sender], "This account doesn't have enogh money to transfer");
+        balance[sender] = SafeMath.sub(balance[sender], amount);
+        balance[reciever] = SafeMath.add(balance[reciever], amount);
+    }
 
 
 }
--- test/functionTest.js
+++ test/functionTest.js
@@ -17,30 +17,31 @@ contract('Contract Test', function (accounts) { //accunts has a list of accounts
         //beforeEach test this function is run
         beforeEach(async function () {
             //a new version of contract is deployed to allow tests to be run on a clean contract
-            testContract = await rounder.new(accounts[2], accounts[3],
+            testContract = await rounder.new(accounts[1], accounts[2],
 
                 {
                     //from account[0]
                     from: deployer             }
             );
+           
         });
 
 
         it('Should set the deployer to owner', async function () {
-            let result = await testContract.owner(); //just a global variable so that's why result is not a transaction value.
+            let result = await testContract.owner();
             assert.equal(result, deployer);
             
         });
 
         it('Should change the address of the owner to newOwner', async function () {
             let newOwner = accounts[3]; //using the address of accounts[3]
-            await testContract.ChangeOwner(newOwner); //<---This is the value of the transaction
+            await testContract.ChangeOwner(newOwner); 
             let result = await testContract.owner();
             assert.equal(result, newOwner);
         });
 
 
-        it('Should set granularity to a value mentioned by user', async function () {
+        it('Should set granularity to a value mentioned by owner', async function () {
             let expectedGranularity = 6
             await testContract.setGranularity(expectedGranularity)
             let result = await testContract.granularity(); 
@@ -48,7 +49,7 @@ contract('Contract Test', function (accounts) { //accunts has a list of accounts
         });
 
 
-        it('Should not allow the granularity to be changed', async function () {
+        it('Should not allow the granularity to be changed as the owner is not calling the fucntion', async function () {
             await truffleAssert.reverts(testContract.setGranularity(
                 6, { from: accounts[3] })
             );
@@ -56,7 +57,7 @@ contract('Contract Test', function (accounts) { //accunts has a list of accounts
         });
 
 
-        it('Should round 7 to granularity', async function () {
+        it('Should round 7 to 10 as the granularity is 10.', async function () {
             let expectedNumber = 10
             await testContract.setGranularity(10) //Need to set a value for granularity in each test
             await testContract.round(7);
@@ -64,21 +65,47 @@ contract('Contract Test', function (accounts) { //accunts has a list of accounts
             assert.equal(output, expectedNumber);
         });
 
-        it('Should round 5 to granularity', async function () {
-            let expectedNumber = 7
-            await testContract.setGranularity(7)
-            await testContract.round(5);
-            let output = await testContract.output();
-            assert.equal(output, expectedNumber);
-        });
-
-        it('Should round 16 to granularity', async function () {
+        it('Should round 16 to 30 as the granularity is 15.', async function () {
             let expectedNumber = 30
             await testContract.setGranularity(15)
             await testContract.round(16);
             let output = await testContract.output();
             assert.equal(output, expectedNumber);
         });
+
+
+        it("Should not allow money to be transferred as the account doesn't have enough money ", async function () {
+            //uisng truffleAssert.revert() to deal the revert from require statements
+            await truffleAssert.reverts(testContract.transferBalance(
+                60, accounts[1], accounts[2])
+            );
+        });
+
+        it("Should not allow transaction to take place as the sender's balance is 0" , async function () {
+            await truffleAssert.reverts(testContract.transferBalance(
+                60, accounts[5], accounts[2])
+            );
+        });
+
+        
+        it('Should transfer money between two accounts by rounding up to the granularity', async function () {
+            //getting the balance of the counterparties
+            let senderBalance = await testContract.balance(accounts[1]);
+            let recieverBalance = await testContract.balance(accounts[2]);
+
+            //setting expected balances after the transaction
+            let expectedBalanceOfReciever = +recieverBalance + +10; 
+            let expectedBalanceOfSender = await +senderBalance - +10;
+
+            await testContract.transferBalance(8, accounts[1], accounts[2]);
+
+            senderBalance = await testContract.balance(accounts[1]);
+            recieverBalance = await testContract.balance(accounts[2]);
+
+            assert.equal(recieverBalance, expectedBalanceOfReciever);
+            assert.equal(senderBalance.toString(), expectedBalanceOfSender.toString());
+        });
+
 
 
     });
